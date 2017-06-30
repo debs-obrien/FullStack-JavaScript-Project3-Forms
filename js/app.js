a6f3b3c8d5c7e5df49fa5928680f8af80aff9f6b
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2017-06-30 11:06:00 +0200

    minor fixes

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 /*
-created by Debbie O'Brien 28 June 2017
-Form validation project for JavaScript Full Stack Project 3
+ created by Debbie O'Brien 28 June 2017
+ Form validation project for JavaScript Full Stack Project 3
  */
 //select elements
 const submit = document.querySelector('button');
@@ -62,129 +62,131 @@ nameField.focus();
 
 //display text field only when Job Role of option is selected
 const addTitle = () => {
-  otherTitle.style.display = "none";
+    otherTitle.style.display = "none";
 
-  jobTitle.addEventListener('change', () => {
-    if (jobTitle.value === 'other') {
-      otherTitle.style.display = "block";
-    } else {
-      otherTitle.style.display = "none";
-      jobValid = true;
-    }
-  });
+    jobTitle.addEventListener('change', () => {
+        if (jobTitle.value === 'other') {
+            otherTitle.style.display = "block";
+        } else {
+            otherTitle.style.display = "none";
+            jobValid = true;
+        }
+    });
 };
 
 
 //hide all other colors only show ones we want
 const hideColors = () => {
-  themeColor.innerHTML = ''; //set all color options to empty so can be refilled based on selection
-  colorDiv.style.display = 'none';
+    themeColor.innerHTML = ''; //set all color options to empty so can be refilled based on selection
+    colorDiv.style.display = 'none';
 
-  themeDesign.addEventListener('change', () => {
-    if (themeDesign.value.includes('Select Theme')) {
-      colorDiv.style.display = 'none';
-    } else {
-      colorDiv.style.display = 'block';
-      if (themeDesign.value.includes('js puns')) {
-        themeColor.innerHTML = '<option value="cornflowerblue">Cornflower Blue (JS Puns shirt only)</option>'
-        themeColor.innerHTML += '<option value="darkslategrey">Dark Slate Grey (JS Puns shirt only)</option>'
-        themeColor.innerHTML += '<option value="gold">Gold (JS Puns shirt only)</option>';
+    themeDesign.addEventListener('change', () => {
+        if (themeDesign.value.includes('Select Theme')) {
+            colorDiv.style.display = 'none';
+        } else {
+            colorDiv.style.display = 'block';
+            if (themeDesign.value.includes('js puns')) {
+                themeColor.innerHTML = '<option value="cornflowerblue">Cornflower Blue (JS Puns shirt only)</option>'
+                themeColor.innerHTML += '<option value="darkslategrey">Dark Slate Grey (JS Puns shirt only)</option>'
+                themeColor.innerHTML += '<option value="gold">Gold (JS Puns shirt only)</option>';
 
-      } else if (themeDesign.value.includes('heart js')) {
-        themeColor.innerHTML = '<option value="tomato">Tomato (I &#9829; JS shirt only)</option>'
-        themeColor.innerHTML += '<option value="steelblue">Steel Blue (I &#9829; JS shirt only)</option>'
-        themeColor.innerHTML += '<option value="dimgrey">Dim Grey (I &#9829; JS shirt only)</option>';
-      }
-    }
+            } else if (themeDesign.value.includes('heart js')) {
+                themeColor.innerHTML = '<option value="tomato">Tomato (I &#9829; JS shirt only)</option>'
+                themeColor.innerHTML += '<option value="steelblue">Steel Blue (I &#9829; JS shirt only)</option>'
+                themeColor.innerHTML += '<option value="dimgrey">Dim Grey (I &#9829; JS shirt only)</option>';
+            }
+        }
 
-  });
+    });
 };
 //check if there are activities selected
 const checkActivities = (checkedOption) => {
-  if (checkedOption.checked) {
-    numOfActivities += 1;
-    activityError.remove();
-  } else {
-    numOfActivities -= 1;
-    activitiesError();
-  }
+    if (checkedOption.checked) {
+        numOfActivities += 1;
+        activityError.remove();
+    } else {
+        numOfActivities -= 1;
+        activitiesError();
+    }
 };
 
 //create error message if no activities selected
 const activitiesError = () => {
-  if (!numOfActivities) {
-    npm.parentNode.insertBefore(activityError, npm.nextSibling.nextSibling);
-    activityError.textContent = 'You need to select an activity';
-    activityError.className = 'error-message';
-    activityValid = false;
-  } else {
-    activityValid = true;
-  }
+    if (!numOfActivities) {
+        npm.parentNode.insertBefore(activityError, npm.nextSibling.nextSibling);
+        activityError.textContent = 'You need to select an activity';
+        activityError.className = 'error-message';
+        activityValid = false;
+    } else {
+        activityValid = true;
+    }
 };
 
 //dont allow selection if same date/time
 const availableActivities = () => {
-  //create div to hold price
-  const activitiesPrice = () => {
-    price.id = 'price';
-      npm.parentNode.insertBefore(price, npm.nextSibling.nextSibling);
-  };
-  //print the price
-  const printPrice = (totalPrice) => {
-    if (totalPrice) {
-      price.innerHTML = 'Total Price: $' + totalPrice;
-    } else {
-      price.innerHTML = '';
+    //create div to hold price
+    const activitiesPrice = () => {
+        price.id = 'price';
+        npm.parentNode.insertBefore(price, npm.nextSibling.nextSibling);
+    };
+    //print the price
+    const printPrice = (totalPrice) => {
+        if (totalPrice) {
+            price.innerHTML = 'Total Price: $' + totalPrice;
+        } else {
+            price.innerHTML = '';
+        }
+    };
+    //calculate the price
+    const calculatePrice = (option, price) => {
+        if (option.checked) {
+            totalPrice += price;
+            printPrice(totalPrice);
+            activityValid = true;
+        } else {
+            totalPrice -= price;
+            printPrice(totalPrice)
+        }
+    };
+    //function to disable checkbox
+    function disable(option) {
+        option.disabled = true;
     }
-  };
-  //calculate the price
-  const calculatePrice = (option, price) => {
-    if (option.checked) {
-      totalPrice += price;
-      printPrice(totalPrice);
-      activityValid = true;
-    } else {
-      totalPrice -= price;
-      printPrice(totalPrice)
-    }
-  };
-  //function to disable checkbox
-  function disable(option) {
-    option.disabled = true;
-  }
-  //function to enable checkbox
-  function enable(option) {
-    option.disabled = false;
-  }
-  //function to check if checked and calculate price
-  const checkIfChecked = (checkedOption, price) => {
-    checkedOption.addEventListener('change', () => {
-      checkedOption.setAttribute("checked", "checked");
-      checkActivities(checkedOption);
-      calculatePrice(checkedOption, price);
-    });
-  };
-  //function to check if checked, disable options of same time and call calculate price
-  const disableIfChecked = (checkedOption, disabledOption) => {
-    checkedOption.addEventListener('change', () => {
-      checkedOption.setAttribute("checked", "checked");
-      checkedOption.checked ? disable(disabledOption) : enable(disabledOption);
-      checkActivities(checkedOption);
-      calculatePrice(checkedOption, workshopPrice);
-    });
-  };
-  //call the activitiesPrice function to append price div
-  activitiesPrice();
 
-  //listen for changes to click and calculate price
-  checkIfChecked(mainConference, mainConfPrice);
-  checkIfChecked(buildTools, workshopPrice);
-  checkIfChecked(npm, workshopPrice);
-  //listen for changes to click, disable checkboxes and calculate price
-  disableIfChecked(jsFrameworks, express);
-  disableIfChecked(jsLibs, node);
-  disableIfChecked(express, jsFrameworks);
-  disableIfChecked(node, jsLibs);
+    //function to enable checkbox
+    function enable(option) {
+        option.disabled = false;
+    }
+
+    //function to check if checked and calculate price
+    const checkIfChecked = (checkedOption, price) => {
+        checkedOption.addEventListener('change', () => {
+            checkedOption.setAttribute("checked", "checked");
+            checkActivities(checkedOption);
+            calculatePrice(checkedOption, price);
+        });
+    };
+    //function to check if checked, disable options of same time and call calculate price
+    const disableIfChecked = (checkedOption, disabledOption) => {
+        checkedOption.addEventListener('change', () => {
+            checkedOption.setAttribute("checked", "checked");
+            checkedOption.checked ? disable(disabledOption) : enable(disabledOption);
+            checkActivities(checkedOption);
+            calculatePrice(checkedOption, workshopPrice);
+        });
+    };
+    //call the activitiesPrice function to append price div
+    activitiesPrice();
+
+    //listen for changes to click and calculate price
+    checkIfChecked(mainConference, mainConfPrice);
+    checkIfChecked(buildTools, workshopPrice);
+    checkIfChecked(npm, workshopPrice);
+    //listen for changes to click, disable checkboxes and calculate price
+    disableIfChecked(jsFrameworks, express);
+    disableIfChecked(jsLibs, node);
+    disableIfChecked(express, jsFrameworks);
+    disableIfChecked(node, jsLibs);
 };
 
 //set payment method to credit card
@@ -192,169 +194,169 @@ paymentMethod.options[1].setAttribute('selected', 'selected');
 
 //hide or show payment methods depending on selection
 const paymentMethods = () => {
-  bitcoinMethod.style.display = 'none';
-  paypalMethod.style.display = 'none';
+    bitcoinMethod.style.display = 'none';
+    paypalMethod.style.display = 'none';
 
-  paymentMethod.addEventListener('change', () => {
-    //if other payment method chosen set credit card Validation to true
-    if (paymentMethod.value === 'bitcoin' || paymentMethod.value === 'paypal') {
-      creditCard.style.display = 'none';
-      ccNumValid = true;
-      zipValid = true;
-      cvvValid = true;
-    }
-    if (paymentMethod.value === 'select_method' || paymentMethod.value === 'credit card') {
-      creditCard.style.display = 'block';
-      bitcoinMethod.style.display = 'none';
-      paypalMethod.style.display = 'none';
-      ccNumValid = false;
-      zipValid = false;
-      cvvValid = false;
-      //hide others
-    } else if (paymentMethod.value === 'paypal') {
-      paypalMethod.style.display = 'block';
-      bitcoinMethod.style.display = 'none';
-    } else if (paymentMethod.value === 'bitcoin') {
-      bitcoinMethod.style.display = 'block';
-      paypalMethod.style.display = 'none';
-    }
-  });
+    paymentMethod.addEventListener('change', () => {
+        //if other payment method chosen set credit card Validation to true
+        if (paymentMethod.value === 'bitcoin' || paymentMethod.value === 'paypal') {
+            creditCard.style.display = 'none';
+            ccNumValid = true;
+            zipValid = true;
+            cvvValid = true;
+        }
+        if (paymentMethod.value === 'select_method' || paymentMethod.value === 'credit card') {
+            creditCard.style.display = 'block';
+            bitcoinMethod.style.display = 'none';
+            paypalMethod.style.display = 'none';
+            ccNumValid = false;
+            zipValid = false;
+            cvvValid = false;
+            //hide others
+        } else if (paymentMethod.value === 'paypal') {
+            paypalMethod.style.display = 'block';
+            bitcoinMethod.style.display = 'none';
+        } else if (paymentMethod.value === 'bitcoin') {
+            bitcoinMethod.style.display = 'block';
+            paypalMethod.style.display = 'none';
+        }
+    });
 };
 
 //empty field validation
 function ifEmpty(input, errorDiv, errorMessage) {
-  if (!input.value) {
-    errorMessages(input, errorDiv, errorMessage);
-  } else {
-    clearErrorMessage(input, errorDiv)
-  }
+    if (!input.value) {
+        errorMessages(input, errorDiv, errorMessage);
+    } else {
+        clearErrorMessage(input, errorDiv)
+    }
 }
 
 //function for error Messages
 function errorMessages(input, errorDiv, errorMessage) {
-  errorDiv.textContent = errorMessage;
-  errorDiv.className = 'error-message';
-  input.className = 'error';
+    errorDiv.textContent = errorMessage;
+    errorDiv.className = 'error-message';
+    input.className = 'error';
 }
 //function to clear error Messages
 function clearErrorMessage(input, errorDiv) {
-  errorDiv.textContent = '';
-  errorDiv.className = '';
-  input.className = '';
+    errorDiv.textContent = '';
+    errorDiv.className = '';
+    input.className = '';
 }
 //validate Name
 const validateName = () => {
-  ifEmpty(nameField, nameError, 'You must fill in in your name');
-  if (nameField.value) {
-    nameValid = true;
-  }
+    ifEmpty(nameField, nameError, 'You must fill in in your name');
+    if (nameField.value) {
+        nameValid = true;
+    }
 };
 //validate email
 const validateEmail = () => {
-  if (regex.test(emailField.value)) {
-    clearErrorMessage(emailField, emailError);
-    emailValid = true;
-  } else {
-    errorMessages(emailField, emailError, 'Please type a valid address eg email@email.com');
-    emailValid = false;
-  }
+    if (regex.test(emailField.value)) {
+        clearErrorMessage(emailField, emailError);
+        emailValid = true;
+    } else {
+        errorMessages(emailField, emailError, 'Please type a valid address eg email@email.com');
+        emailValid = false;
+    }
 };
 //validate job Role
 const validateJob = () => {
-  if (jobTitle.value === 'other' && !otherTitle.value) {
-      errorMessages(otherTitle, jobError, 'You must fill in your Job Role');
+    if (jobTitle.value === 'other' && !otherTitle.value) {
+        errorMessages(otherTitle, jobError, 'You must fill in your Job Role');
         jobValid = false;
-  }else {
-      clearErrorMessage(otherTitle, jobError);
-      jobValid = true;
-  }
+    } else {
+        clearErrorMessage(otherTitle, jobError);
+        jobValid = true;
+    }
 };
 
 //validate cc Number
 const validateCCNum = () => {
-  if (isNaN(ccNumField.value)) {
-    errorMessages(ccNumField, ccNumError, 'Credit Card numbers need to be a number');
-  } else if (ccNumField.value.length < 13 || ccNumField.value.length > 16) {
-    errorMessages(ccNumField, ccNumError, 'Your credit car number needs to be min 13 digits and max 16 digits');
-  } else {
-    clearErrorMessage(ccNumField, ccNumError);
-    ccNumValid = true;
-  }
+    if (isNaN(ccNumField.value)) {
+        errorMessages(ccNumField, ccNumError, 'Credit Card numbers need to be a number');
+    } else if (ccNumField.value.length < 13 || ccNumField.value.length > 16) {
+        errorMessages(ccNumField, ccNumError, 'Your credit car number needs to be min 13 digits and max 16 digits');
+    } else {
+        clearErrorMessage(ccNumField, ccNumError);
+        ccNumValid = true;
+    }
 };
 //validate zip
 const validateZip = () => {
-  if (isNaN(zipField.value)) {
-    errorMessages(zipField, zipError, 'zips need to be a number');
-  } else if (zipField.value.length < 5 || zipField.value.length > 5) {
-    errorMessages(zipField, zipError, 'Your zip code needs to be min 5 digits and max 5 digits');
-  } else {
-    clearErrorMessage(zipField, zipError);
-    zipValid = true;
-  }
+    if (isNaN(zipField.value)) {
+        errorMessages(zipField, zipError, 'zips need to be a number');
+    } else if (zipField.value.length < 5 || zipField.value.length > 5) {
+        errorMessages(zipField, zipError, 'Your zip code needs to be min 5 digits and max 5 digits');
+    } else {
+        clearErrorMessage(zipField, zipError);
+        zipValid = true;
+    }
 };
 //validate cvv
 const validateCvv = () => {
-  if (isNaN(cvvField.value)) {
-    errorMessages(cvvField, cvvError, 'cvv needs to be a number');
-  } else if (cvvField.value.length < 3 || cvvField.value.length > 3) {
-    errorMessages(cvvField, cvvError, 'Your cvv number needs to be 3 digits');
-  } else {
-    clearErrorMessage(cvvField, cvvError);
-    cvvValid = true;
-  }
+    if (isNaN(cvvField.value)) {
+        errorMessages(cvvField, cvvError, 'cvv needs to be a number');
+    } else if (cvvField.value.length < 3 || cvvField.value.length > 3) {
+        errorMessages(cvvField, cvvError, 'Your cvv number needs to be 3 digits');
+    } else {
+        clearErrorMessage(cvvField, cvvError);
+        cvvValid = true;
+    }
 };
 
 nameField.addEventListener('blur', () => {
-  validateName();
+    validateName();
 });
 emailField.addEventListener('blur', () => {
-  validateEmail();
+    validateEmail();
 });
 jobTitle.addEventListener('change', () => {
-  if(jobTitle.value === 'other'){
-    jobValid = false;
-  }else{
-      clearErrorMessage(otherTitle, jobError);
-      jobValid = true;
-  }
+    if (jobTitle.value === 'other') {
+        jobValid = false;
+    } else {
+        clearErrorMessage(otherTitle, jobError);
+        jobValid = true;
+    }
 });
 otherTitle.addEventListener('blur', () => {
-  validateJob();
+    validateJob();
 });
 ccNumField.addEventListener('blur', () => {
-  validateCCNum();
+    validateCCNum();
 });
 zipField.addEventListener('blur', () => {
-  validateZip();
+    validateZip();
 });
 cvvField.addEventListener('blur', () => {
-  validateCvv();
+    validateCvv();
 });
 //validate form
 const validateFrom = () => {
-  activitiesError();
-  ifEmpty(nameField, nameError, 'You must fill in in your name');
-  ifEmpty(emailField, emailError, 'You must fill in in your email');
-  ifEmpty(ccNumField, ccNumError, 'you must add your credit card number or choose another payment type');
-  ifEmpty(zipField, zipError, 'you must add your zip number');
-  ifEmpty(cvvField, cvvError, 'you must add your cvv number');
-  //if fields are not empty validate further
-  if (emailField.value) {
-    validateEmail();
-  }
-  if (ccNumField.value) {
-    validateCCNum();
-  }
-  if (zipField.value) {
-    validateZip();
-  }
-  if (cvvField.value) {
-    validateCvv();
-  }
-  if (jobTitle.value === 'other'){
-    jobValid = false;
-    validateJob();
-  }
+    activitiesError();
+    ifEmpty(nameField, nameError, 'You must fill in in your name');
+    ifEmpty(emailField, emailError, 'You must fill in in your email');
+    ifEmpty(ccNumField, ccNumError, 'you must add your credit card number or choose another payment type');
+    ifEmpty(zipField, zipError, 'you must add your zip number');
+    ifEmpty(cvvField, cvvError, 'you must add your cvv number');
+    //if fields are not empty validate further
+    if (emailField.value) {
+        validateEmail();
+    }
+    if (ccNumField.value) {
+        validateCCNum();
+    }
+    if (zipField.value) {
+        validateZip();
+    }
+    if (cvvField.value) {
+        validateCvv();
+    }
+    if (jobTitle.value === 'other') {
+        jobValid = false;
+        validateJob();
+    }
 };
 
 //call all functions
@@ -365,10 +367,10 @@ paymentMethods();
 
 //submit form on click if everything validates
 submit.addEventListener('click', (e) => {
-  //let allFields = fields required to be validated
-  let allValid = nameValid && emailValid && jobValid && activityValid && zipValid && ccNumValid && cvvValid;
-  if (!allValid) { //if not true prevent submit and call validate
-    e.preventDefault();
-    validateFrom();
-  }
+    //let allFields = fields required to be validated
+    let allValid = nameValid && emailValid && jobValid && activityValid && zipValid && ccNumValid && cvvValid;
+    if (!allValid) { //if not true prevent submit and call validate
+        e.preventDefault();
+        validateFrom();
+    }
 });
